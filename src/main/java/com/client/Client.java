bef3294a4815b830737ec9f78e11cb341ffe98f5
DhineshKannan97 <dhineshkannan.madhavan@gcitsolutions.com>
2022-12-13 20:10:06 +0530

    stage 1

--- src/main/java/com/client/Client.java
+++ src/main/java/com/client/Client.java
@@ -1,39 +1,43 @@
 package com.client;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+
 public class Client {
-    public void clientServer(String ip ,int port) throws IOException {
-        Socket s ;
-        DataOutputStream out ;
+    public void clientServer(String ip, int port) throws IOException {
+        Socket s;
+        DataOutputStream out;
         DataInputStream input;
-        try{
-            s= new Socket(ip,port);
-            input =new DataInputStream(System.in);
-            out=new DataOutputStream(s.getOutputStream());
+        try {
+            s = new Socket(ip, port);
+            input = new DataInputStream(System.in);
+            out = new DataOutputStream(s.getOutputStream());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        String line="";
-        while (!line.equals("end")){
-            try{
+        String line = "";
+        while (!line.equals("end")) {
+            try {
                 line = input.readLine();
                 out.writeUTF(line);
+                System.out.println(line);
             } catch (IOException e) {
-                s.close();
                 throw new RuntimeException(e);
             }
         }
-        try{
+        try {
             s.close();
+            input.close();
+            out.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     public static void main(String[] args) throws IOException {
-        Client client1=new Client();
-        client1.clientServer("localhost",5000);
+        Client client1 = new Client();
+        client1.clientServer("localhost", 5000);
     }
 }
